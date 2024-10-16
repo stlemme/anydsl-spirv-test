d6d7b65291ffd06b6af32f55372e85946de04f90
Stefan Lemme <stefan.lemme@dfki.de>
2024-10-16 09:33:53 +0000

    avoid reusing memory allocations to improve sample robustness

--- copy-kernel.cpp
+++ copy-kernel.cpp
@@ -12,16 +12,24 @@ int main(int argc, const char** argv) {
 	uint32_t s = n * sizeof(uint32_t);
 
 	void* p0 = anydsl_alloc_host(dev, s);
-	void* p1 = anydsl_alloc(dev, s);
-	void* p2 = anydsl_alloc_unified(dev, s);
+ 	void* p1 = anydsl_alloc(dev, s);
+	//void* p2 = anydsl_alloc_unified(dev, s);
 	void* p3 = anydsl_alloc(dev, s);
+	void* p4 = anydsl_alloc_host(dev, s);
 
-	uint32_t* p = static_cast<uint32_t*>(p0);
+	uint32_t* pp0 = static_cast<uint32_t*>(p0);
+	uint32_t* pp4 = static_cast<uint32_t*>(p4);
+
 	for (uint32_t i = 0; i < n; ++i) {
-		p[i] = i;
+		pp0[i] = i;
+		pp4[i] = 0x23;
 	}
 
 	anydsl_copy(dev, p0, 0, dev, p1, 0, s);
+
+        //for (uint32_t i = 0; i < n; ++i) {
+        //        p[i] = 0x0;
+        //}
 
 	const char filename[] = KERNEL_PATH "/spv_kernels.spv";
 	const char kernelname[] = "copy_krnl";
@@ -31,7 +39,7 @@ int main(int argc, const char** argv) {
 	uint32_t grid[3] = { n, 1, 1 };
 	uint32_t block[3] = { 32, 1, 1 };
 
-	void* arg_data[2] = { p1, p3 };
+	void* arg_data[2] = { &p1, &p3 };
 	const uint32_t arg_sizes[2] = { sizeof(uint32_t*), sizeof(uint32_t*) };
 	const uint32_t arg_aligns[2] = { 0, 0 };
 	const uint32_t arg_alloc_sizes[2] = { sizeof(uint32_t*), sizeof(uint32_t*) };
@@ -40,15 +48,20 @@ int main(int argc, const char** argv) {
 
 	anydsl_launch_kernel(dev, filename, kernelname, grid, block, arg_data, arg_sizes, arg_aligns, arg_alloc_sizes, arg_types, num_args);
 
-	anydsl_copy(dev, p3, 0, dev, p0, 0, s);
+	anydsl_copy(dev, p3, 0, dev, p4, 0, s);
+
+	anydsl_synchronize(dev);
 
 	for (uint32_t i = 0; i < 256; ++i) {
-		std::cout << i << ": " << p[i] << std::endl;
+		std::cout << i << ": " << pp4[i] << std::endl;
 	}
 
+	//anydsl_synchronize(dev);
+
 	anydsl_release(dev, p3);
-	anydsl_release(dev, p2);
+	//anydsl_release(dev, p2);
 	anydsl_release(dev, p1);
+	anydsl_release_host(dev, p4);
 	anydsl_release_host(dev, p0);
 
 	return 0;
--- copy.cpp
+++ copy.cpp
@@ -7,7 +7,7 @@ int main(int argc, const char** argv) {
 	anydsl_info();
 
 	int32_t dev = ANYDSL_DEVICE(ANYDSL_LEVELZERO, 0);
-	int32_t cpu = ANYDSL_DEVICE(ANYDSL_HOST, 0);
+	//int32_t cpu = ANYDSL_DEVICE(ANYDSL_HOST, 0);
 
 	uint32_t n = 8192;
 	uint32_t s = n * sizeof(uint32_t);
@@ -16,20 +16,29 @@ int main(int argc, const char** argv) {
 	//void* p0 = anydsl_alloc(cpu, s);
 	void* p1 = anydsl_alloc(dev, s);
 	void* p2 = anydsl_alloc_unified(dev, s);
+	void* p3 = anydsl_alloc_host(dev, s);
 
-	uint32_t* p = static_cast<uint32_t*>(p0);
+	uint32_t* pp0 = static_cast<uint32_t*>(p0);
+	uint32_t* pp3 = static_cast<uint32_t*>(p3);
+
 	for (uint32_t i = 0; i < n; ++i) {
-		p[i] = i;
+		pp0[i] = i;
+		pp3[i] = 0x23;
 	}
 
-	anydsl_copy(cpu, p0, 0, dev, p1, 0, s);
+	anydsl_copy(dev, p0, 0, dev, p1, 0, s);
 
-	anydsl_copy(dev, p1, 0, cpu, p0, 0, s);
+	anydsl_copy(dev, p1, 0, dev, p2, 0, s);
+
+	anydsl_copy(dev, p2, 0, dev, p3, 0, s);
+
+	anydsl_synchronize(dev);
 
 	for (uint32_t i = 0; i < 256; ++i) {
-		std::cout << i << ": " << p[i] << std::endl;
+		std::cout << i << ": " << pp3[i] << std::endl;
 	}
 
+	anydsl_release_host(dev, p3);
 	anydsl_release(dev, p2);
 	anydsl_release(dev, p1);
 	anydsl_release_host(dev, p0);
