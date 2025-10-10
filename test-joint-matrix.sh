e27f5dfd14042764d40fc270d1f4768be93a5987
Stefan Lemme <stefan.lemme@dfki.de>
2025-10-10 11:47:11 +0200

    add environ settings to dump IGC shader code

--- test-joint-matrix.sh
+++ test-joint-matrix.sh
@@ -4,6 +4,11 @@ sycl-ls
 
 sycl-ls --verbose | grep "ext_intel_matrix"
 
+export NEO_CACHE_PERSISTENT=0
+export IGC_ShaderDumpEnable=1
+export IGC_ShaderDumpEnableAll=0
+export IGC_DumpToCustomDir=$(pwd)/shader_dump
+
 icpx -fsycl -fsycl-targets=spir64_gen -o joint-matrix -Xsycl-target-backend "-device dg2 -internal_options -ze-opt-large-register-file" joint-matrix.cpp
 
 ONEAPI_DEVICE_SELECTOR=level_zero:1 ./joint-matrix
